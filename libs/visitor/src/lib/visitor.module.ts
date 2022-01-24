9046f8418f41cc8ad4f2414135d98c8b60cf9461
vendingcompute <edutre1999@gmail.com>
2022-01-24 01:32:23 -0600

    Generated 'visitor' component. Added it to 'visitor' library

--- libs/visitor/src/lib/visitor.module.ts
+++ libs/visitor/src/lib/visitor.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { VisitorComponent } from './visitor.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, LayoutModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule],
+  declarations: [VisitorComponent],
 })
 export class VisitorModule {}
