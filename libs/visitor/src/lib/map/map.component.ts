46af8ce233e190ead9b89dbf89e9357ee16c431f
vendingcompute <edutre1999@gmail.com>
2022-01-24 14:14:21 -0600

    Implemented map functionality

--- libs/visitor/src/lib/map/map.component.ts
+++ libs/visitor/src/lib/map/map.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { PoiSelectors } from '@packt/poi';
 
 @Component({
   selector: 'packt-map',
@@ -6,8 +8,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
+  poi$ = this.store.select(PoiSelectors.getSelected);
 
-  constructor() { }
+  constructor(private store: Store) { }
 
   ngOnInit(): void {
   }
