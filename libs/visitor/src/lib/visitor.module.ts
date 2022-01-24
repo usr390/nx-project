891030f4e36a7e9108aa02a18de7bd9fb295195d
vendingcompute <edutre1999@gmail.com>
2022-01-24 01:36:54 -0600

    Routed 'visitor' component. App defaults to this component when loaded

--- libs/visitor/src/lib/visitor.module.ts
+++ libs/visitor/src/lib/visitor.module.ts
@@ -7,9 +7,14 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [CommonModule, LayoutModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule],
+  imports: [CommonModule, LayoutModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, RouterModule.forChild([
+
+    {path: '', component: VisitorComponent}
+
+  ])],
   declarations: [VisitorComponent],
 })
 export class VisitorModule {}
