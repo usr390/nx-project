4b3a495fe06392887e181ac21f6ba95ae8e5a3d5
vendingcompute <edutre1999@gmail.com>
2022-01-24 01:53:44 -0600

    Routed 'admin' component.

--- libs/admin/src/lib/admin.module.ts
+++ libs/admin/src/lib/admin.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminComponent } from './admin.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule.forChild([
+
+    { path: '', component: AdminComponent }
+
+  ])],
   declarations: [
     AdminComponent
   ],
