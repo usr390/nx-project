06c67e13f41089b53f6e09352aaaeeecd555097d
vendingcompute <edutre1999@gmail.com>
2022-01-24 02:11:40 -0600

    Added all necessary NgRx npm packages. Modified 'app.module.ts' to configure NgRx-related artifacts

--- apps/tour/src/app/app.module.ts
+++ apps/tour/src/app/app.module.ts
@@ -4,16 +4,43 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, RouterModule.forRoot([
-
-    { path: 'admin', loadChildren: () => import('@packt/admin').then(m => m.AdminModule)},
-    { path: 'tour', loadChildren: () => import('@packt/visitor').then(m => m.VisitorModule)},
-    { path: '', pathMatch: 'full', redirectTo: 'tour'}
-
-  ])],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot([
+      {
+        path: 'admin',
+        loadChildren: () => import('@packt/admin').then((m) => m.AdminModule),
+      },
+      {
+        path: 'tour',
+        loadChildren: () =>
+          import('@packt/visitor').then((m) => m.VisitorModule),
+      },
+      { path: '', pathMatch: 'full', redirectTo: 'tour' },
+    ]),
+    StoreModule.forRoot(
+      {},
+      {
+        metaReducers: !environment.production ? [] : [],
+        runtimeChecks: {
+          strictActionImmutability: true,
+          strictStateImmutability: true,
+        },
+      }
+    ),
+    EffectsModule.forRoot([]),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    StoreRouterConnectingModule.forRoot(),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
